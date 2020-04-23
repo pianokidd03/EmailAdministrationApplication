bb9c44b55f2b425f6dd55009c0276923d9e65454
The Piano Kid <43418935+pianokidd03@users.noreply.github.com>
2020-04-23 15:32:07 -0400

    Init

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.gabebraden;
+
+// Many thanks to the Master Skills (https://www.youtube.com/channel/UCJDMU2Z2A70Yu3pVe_c-HYw)
+// for the challenge description and solution
+
+public class Main {
+
+    public static void main(String[] args) {
+        Email email1 = new Email("Steampunk Robotics","Gabe", "Braden");
+    }
+}
